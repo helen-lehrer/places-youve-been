21e8a9e0119b7fb34150418252304a584cf5b566
Helen Lehrer <hlehrer@outlook.com>
2022-08-22 15:04:58 -0700

    WIP UI logic
    co-authored-by: Shaniza Lingle <shanizalingle@gmail.com>
    Co-authored-by: Harold Mesa <harold.mesa@icloud.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
-// Business logic for directory
-function LocationDirectory() {
-  this.locations = {};
-}
+// Business logic for locations
 
-LocationDirectory.prototype.addLocation = function(place) {
+function submitPlace () { 
+  // Business logic for directory
+  function LocationDirectory() {
+    this.locations = {};
+  }
+
+  LocationDirectory.prototype.addLocation = function(place) {
   this.locations[place.name] = place;
+  }
 }
 
 LocationDirectory.prototype.findLocation = function(name) {
@@ -22,11 +26,30 @@ LocationDirectory.prototype.deleteLocation = function(name) {
   return true;
 }
 
-// Business logic for locations
 function Place(name, year, landmark, notes) {
   this.name = name;
   this.year = year;
   this.landmark = landmark;
   this.notes = notes;
 }
+
+// UI Logic
+
+
+function handleSubmission(event) {
+  event.preventDefault();
+  submitPlace;
+
+  // get input values
+  const nameInput = document.getElementById("nameOfPlace").value;
+  const yearInput = document.getElementById("year").value;
+  const landmarkInput = document.getElementById("landmark").value;
+  const notesInput = document.getElementById("notes").value;
+
+}
+
+window.addEventListener ("load", function {
+  let form = document.getElementById("inputPlace");
+  form.addEventListener("submit", submitPlace()); 
+})
 
